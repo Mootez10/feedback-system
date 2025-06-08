08a6b3a0216c3d7cf4e5fae2820074f829117232
Mootez <mootezaouinti1@gmail.com>
2025-06-08 03:23:19 +0200

    product entity done

--- src/product/product.resolver.ts
+++ src/product/product.resolver.ts
@@ -1,4 +1,25 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { ProductService } from './product.service';
+import { Product } from './product.entity';
+import { CreateProductInput } from './dto/create-product.input';
+import { GraphQLBoolean } from 'graphql';
 
-@Resolver()
-export class ProductResolver {}
+@Resolver(() => Product)
+export class ProductResolver {
+  constructor(private productService: ProductService) {}
+
+  @Query(() => [Product])
+  getAllProducts() {
+    return this.productService.findAll();
+  }
+
+  @Mutation(() => Product)
+  addProduct(@Args('input') input: CreateProductInput) {
+    return this.productService.create(input);
+  }
+
+  @Mutation(() => GraphQLBoolean)
+  deleteProduct(@Args('id', { type: () => Int }) id: number) {
+    return this.productService.delete(id);
+  }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './product.entity';
+import { CreateProductInput } from './dto/create-product.input';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  private products: Product[] = [];
+  private idCounter = 1;
+
+  create(input: CreateProductInput): Product {
+    const newProduct = { id: this.idCounter++, ...input };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  findAll(): Product[] {
+    return this.products;
+  }
+
+  delete(id: number): boolean {
+    const index = this.products.findIndex(p => p.id === id);
+    if (index === -1) return false;
+    this.products.splice(index, 1);
+    return true;
+  }
+}
